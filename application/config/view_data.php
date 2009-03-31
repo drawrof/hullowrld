5811f072720ba32bb3ba041f84693475f09426e9
Drawrof <drawrof@gmail.com>
2009-03-30 21:35:53 -0400

    Added missing files. I'm still learning git.

---
+++ application/config/view_data.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['home/index'] = array(
+		'title' => 'hellowrld - a simple PHP framework',
+	);
+	
+$config['_default'] = array(
+		'title' => 'Default Page Title - Will be overridden by the above array when viewing the home page'
+	);
